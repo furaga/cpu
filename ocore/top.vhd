97ea79738c4d15efd85b92dfe820e614f191ded3
Tomoyuki Saito <aocchoda@gmail.com>
2012-02-07 23:47:47 +0900

    io for fib 9

--- ocore/top.vhd
+++ ocore/top.vhd
@@ -28,19 +28,24 @@ signal clk,iclk: std_logic;
 		);				
 	end component;
 
-	--component u232c is
-	  --generic (wtime: std_logic_vector(15 downto 0) := x"1ADB");
-	  --Port ( clk  : in  STD_LOGIC;
-			 --data : in  STD_LOGIC_VECTOR (7 downto 0);
-			 ----go   : in  STD_LOGIC;
-			 --busy : out STD_LOGIC;
-			 --tx   : out STD_LOGIC);
-	--end component;
+	component u232c is
+	  generic (wtime: std_logic_vector(15 downto 0) := x"0001");
+	  Port ( clk  : in  STD_LOGIC;
+			 data : in  STD_LOGIC_VECTOR (7 downto 0);
+			 go   : in  STD_LOGIC;
+			 busy : out STD_LOGIC;
+			 tx   : out STD_LOGIC);
+	end component;
 
 	signal reset : std_logic := '1';
 	signal send_busy : std_logic;
-	signal send_go : std_logic := '1';
+	signal send_go : std_logic;
+	signal send_in : std_logic_vector(31 downto 0);
 	signal send_data : std_logic_vector(31 downto 0);
+	signal send_buf0 : std_logic_vector(31 downto 0);
+	signal send_buf1 : std_logic_vector(31 downto 0);
+	signal buf_count : std_logic_vector(3 downto 0) := "0000";
+	signal output_count : std_logic_vector(3 downto 0) := "0000";
 
 begin
 
@@ -60,10 +65,38 @@ begin
 				IO65_IN  => (others=>'0'),
 				IO64_OUT  => send_data);
 
-	--send_u : u232c port map (
-				--clk =>clk,
-				--data=>send_data(7 downto 0),
-				--go=>send_go,
-				--busy=>send_busy,
-				--tx=>RS_TX);
+	count : process(send_data)
+	begin
+		if (send_data'event) then
+			buf_count <= buf_count + 1;
+			if buf_count = "0000" then
+				send_buf0 <= send_data;
+			else
+				send_buf1 <= send_data;
+			end if;
+		end if;
+	end process;
+
+	send_in <= send_buf0 when output_count = "0000" else send_buf1;
+
+	send_msg : process(clk)
+	begin
+		if (rising_edge(clk)) then
+			if ((buf_count>output_count) and send_busy='0') then
+				send_go <= '1';
+			elsif send_go='1' and send_busy='1' then
+				output_count <= output_count + 1;
+				send_go <= '0';
+			else -- send_go <= '0'
+				send_go <= '0';
+			end if;
+		end if;
+	end process;
+
+	send_u : u232c port map (
+				clk =>clk,
+				data=>send_in(7 downto 0),
+				go=>send_go,
+				busy=>send_busy,
+				tx=>RS_TX);
 end cpu;
